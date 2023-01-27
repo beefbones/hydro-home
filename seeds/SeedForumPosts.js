5e7b64ba286c8d525a287de73600e49867eff006
Christian Kester <christiankester77@gmail.com>
2023-01-26 21:03:49 -0500

    added and tested all seed data

--- seeds/SeedForumPosts.js
+++ seeds/SeedForumPosts.js
@@ -3,16 +3,46 @@ const { ForumPost } = require("../models");
 
 const seedForumPosts = async () => {
     await ForumPost.bulkCreate([
-        { id: 1, title: "Water is the best", content: "TestContent1", user_id: 1, timestamp: new Date(2023, 0, 24, 12, 1, 1) },
-        { id: 2, title: "Dasani is actually terrible", content: "TestContent2", user_id: 2, timestamp: new Date(2023, 0, 24, 13, 2, 2) },
-        { id: 3, title: "Can anyone actually afford Fiji???", content: "TestContent3", user_id: 3, timestamp: new Date(2023, 0, 24, 14, 3, 3) },
-        { id: 4, title: "TestTitle 4", content: "TestContent4", user_id: 4, timestamp: new Date(2023, 0, 24, 15, 4, 4) },
-        { id: 5, title: "TestTitle 5", content: "TestContent5", user_id: 5, timestamp: new Date(2023, 0, 24, 16, 5, 5) },
-        { id: 6, title: "TestTitle 6", content: "TestContent6", user_id: 5, timestamp: new Date(2023, 0, 24, 17, 6, 6) },
-        { id: 7, title: "TestTitle 7", content: "TestContent7", user_id: 4, timestamp: new Date(2023, 0, 24, 18, 1, 1) },
-        { id: 8, title: "TestTitle 8", content: "TestContent8", user_id: 3, timestamp: new Date(2023, 0, 24, 19, 2, 2) },
-        { id: 9, title: "TestTitle 9", content: "TestContent9", user_id: 2, timestamp: new Date(2023, 0, 24, 20, 3, 3) },
-        { id: 10, title: "TestTitle 10", content: "TestContent10", user_id: 2, timestamp: new Date(2023, 0, 24, 21, 4, 4) },
+        { id: 1, post_title: "Water is the best", content: "I love hydration", user_id: 1, timestamp: new Date(2023, 0, 24, 12, 1, 1) },
+        {
+            id: 2,
+            post_title: "Dasani is actually terrible",
+            content: "Coca cola company stinks :(",
+            user_id: 2,
+            timestamp: new Date(2023, 0, 24, 13, 2, 2),
+        },
+        {
+            id: 3,
+            post_title: "Can anyone actually afford Fiji???",
+            content: "Water should be free",
+            user_id: 3,
+            timestamp: new Date(2023, 0, 24, 14, 3, 3),
+        },
+        { id: 4, post_title: "I actually enjoy tap water", content: "Please dont judge me", user_id: 4, timestamp: new Date(2023, 0, 24, 15, 4, 4) },
+        { id: 5, post_title: "Water is good", content: "I like it", user_id: 5, timestamp: new Date(2023, 0, 24, 16, 5, 5) },
+        {
+            id: 6,
+            post_title: "I just hit my hydration goal!!",
+            content: "LETS GOOOOOOOO!!!!!!",
+            user_id: 5,
+            timestamp: new Date(2023, 0, 24, 17, 6, 6),
+        },
+        {
+            id: 7,
+            post_title: "I didn't hit my hydration goals :(",
+            content: "(I will tomorrow)",
+            user_id: 4,
+            timestamp: new Date(2023, 0, 24, 18, 1, 1),
+        },
+        {
+            id: 8,
+            post_title: "Is it possible to drink too much water?",
+            content: "Can too much of a good thing be a bad thing?",
+            user_id: 3,
+            timestamp: new Date(2023, 0, 24, 19, 2, 2),
+        },
+        { id: 9, post_title: "WATER", content: "I LIKE IT", user_id: 2, timestamp: new Date(2023, 0, 24, 20, 3, 3) },
+        { id: 10, post_title: "I drink a gallon of water a day", content: "ama", user_id: 2, timestamp: new Date(2023, 0, 24, 21, 4, 4) },
     ]);
 };
 
