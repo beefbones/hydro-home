7af1b5eb2594e92a4f4c0bf4cc2f9726fe9f60bc
Jakob Dayton <jakobdayton@gmail.com>
2023-01-26 19:30:49 -0500

    added index.js for models

--- models/index.js
+++ models/index.js
@@ -1,2 +1,18 @@
-const sequelize = require("./connection");
-const User = require("")
+const User = require("./User");
+const WaterConsumption = require("./WaterConsumption");
+const ForumPost = require("./ForumPost");
+const Comment = require("./Comment");
+
+User.hasMany(ForumPost, { foreignKey: "user_id" });
+ForumPost.belongsTo(User, { foreignKey: "user_id" });
+
+User.hasMany(Comment, { foreignKey: "user_id" });
+Comment.belongsTo(User, { foreignKey: "user_id" });
+
+User.hasMany(WaterConsumption, { foreignKey: "user_id"});
+WaterConsumption.belongsTo(User, { foreignKey: "user_id"});
+
+ForumPost.hasMany(Comment, {foreignKey: "post_id"});
+Comment.belongsTo(ForumPost, {foreignKey: "post_id"});
+
+models.exports = {User, WaterConsumption, ForumPost, Comment};
