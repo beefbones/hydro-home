60a9028262648e84c732f99adfc6fc362018fb2d
Christian Kester <christiankester77@gmail.com>
2023-01-26 21:03:49 -0500

    added and tested all seed data

--- seeds/SeedUsers.js
+++ seeds/SeedUsers.js
@@ -3,11 +3,11 @@ const { User } = require("../models");
 
 const seedUsers = async () => {
     await User.bulkCreate([
-        { id: 1, username: "testUser1", hashed_password: "password1231" },
-        { id: 2, username: "testUser2", hashed_password: "password1232" },
-        { id: 3, username: "testUser3", hashed_password: "password1233" },
-        { id: 4, username: "testUser4", hashed_password: "password1234" },
-        { id: 5, username: "testUser5", hashed_password: "password1235" },
+        { id: 1, username: "Liam Smith", hashed_password: "password1231", email: "Lsmith@gmail.com" },
+        { id: 2, username: "Noah Roberts", hashed_password: "password1232", email: "nRoberts@gmail.com" },
+        { id: 3, username: "Oliver Williams", hashed_password: "password1233", email: "testemail1@gmail.com" },
+        { id: 4, username: "Emma Brown", hashed_password: "password1234", email: "Emma@gmail.com" },
+        { id: 5, username: "Ashley Smith", hashed_password: "password1235", email: "AshleySmith@gmail.com" },
     ]);
 };
 
