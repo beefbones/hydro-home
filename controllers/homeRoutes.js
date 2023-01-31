6e8259a543e53b23feb9a5ec55d8185eeb61f96f
Nolan Ross <nolanross917@gmail.com>
2023-01-30 19:43:40 -0500

    created routes in userRoutes & homeRoutes, added authentication in utils

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -3,16 +3,13 @@ const { User } = require('../models');
 
 router.get('/', async (req, res) => {
   try {
-    // Get all users, sorted by name
     const userData = await User.findAll({
       attributes: { exclude: ['password'] },
       order: [['username', 'ASC']],
     });
 
-    // Serialize user data so templates can read it
     const users = userData.map((project) => project.get({ plain: true }));
 
-    // Pass serialized data into Handlebars.js template
     res.render('homepage', { users });
   } catch (err) {
     res.status(500).json(err);
