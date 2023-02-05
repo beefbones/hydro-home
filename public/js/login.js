7f97feb92683d2aa4efb8c214e3d387903a2df2a
Christian Kester <christiankester77@gmail.com>
2023-02-04 20:38:54 -0500

    fixed signup form

--- public/js/login.js
+++ public/js/login.js
@@ -1,25 +1,22 @@
 const loginFormHandler = async (event) => {
     event.preventDefault();
-  
-    const email = document.querySelector('#email-login').value.trim();
-    const password = document.querySelector('#password-login').value.trim();
-  
-    if (email && password) {
-      const response = await fetch('/api/users/login', {
-        method: 'POST',
-        body: JSON.stringify({ email, password }),
-        headers: { 'Content-Type': 'application/json' },
-      });
-  
-      if (response.ok) {
-        document.location.replace('/');
-      } else {
-        alert('Failed to log in');
-      }
+
+    const loginId = document.querySelector("#login-id").value.trim();
+    const password = document.querySelector("#login-password").value.trim();
+
+    if (loginId && password) {
+        const response = await fetch("/api/users/login", {
+            method: "POST",
+            body: JSON.stringify({ email: loginId, username: loginId, password }),
+            headers: { "Content-Type": "application/json" },
+        });
+
+        if (response.ok) {
+            document.location.replace("/");
+        } else {
+            alert("Failed to log in");
+        }
     }
-  };
-  
-  document
-    .querySelector('.login-form')
-    .addEventListener('submit', loginFormHandler);
-  
+};
+
+document.querySelector(".login-form").addEventListener("submit", loginFormHandler);
